07e40d465df0ea4405cc890af17cba6de51c524d
Satyajitkumar123 <satyajitkumar2472@gmail.com>
2026-04-02 18:13:18 +0000

    feat: add User model for authentication in users app

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models 
 from django.utils.translation import gettext_lazy as _ 
 
@@ -27,3 +27,26 @@ class CustomUserManager(BaseUserManager):
 
         return self.create_user(email, password, **extra_fields)
     
+class User(AbstractUser):
+    """Custom user model with email as the unique identifier."""
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first_name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last_name"), max_length=30, blank=True)
+    phone_number = models.CharField(_("phone number"), max_length=15, blank=True)
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"), auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = CustomUserManager()
+    
+    class Meta:
+        db_table = "users"
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
+
+    def __str__(self):
+        return self.email
